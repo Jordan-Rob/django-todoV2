a91612aa8d2b5b2314ab704083d9e4e080cc3778
jordanrob709 <jordanrob709@gmail.com>
2020-03-12 15:02:27 +0300

    creating todo_list template

--- todos/views.py
+++ todos/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.views.generic import (
+    CreateView,
+    ListView,
+    DetailView,
+    UpdateView,
+    DeleteView
+)
+
+from django.urls import reverse_lazy
+from .models import Todo
 
 # Create your views here.
+
+
+class TodoCreateView(CreateView):
+    model = Todo
+    fields = ('title', 'description', 'to_be_done')
+    template_name = 'todo_new.html'
+
+
+class TodoListView(ListView):
+    model = Todo
+    template_name = 'todo_list.html'
+
+
+class TodoDetailView(DetailView):
+    model = Todo
+    template_name = 'todo_detail.html'
+
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ('title', 'description', 'to_be_done')
+    template_name = 'todo_edit.html'
+
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    template_name = 'todo_delete.html'
+    success_url = reverse_lazy('todo_list')
