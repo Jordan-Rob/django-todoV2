512a8cb051efabcdef3d4d8ac68f8c52fcc50bf4
jordanrob709 <jordanrob709@gmail.com>
2020-03-12 12:09:08 +0300

    added todos app

--- todoII/settings.py
+++ todoII/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'todos.apps.TodosConfig',
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
