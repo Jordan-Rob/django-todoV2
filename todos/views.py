e86aea0309f89aae252d733203b22f6f7baaed65
jordanrob709 <jordanrob709@gmail.com>
2020-03-13 15:34:37 +0300

    adding authorizations

--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
 from django.views.generic import (
     CreateView,
@@ -14,33 +15,38 @@ from .models import Todo
 # Create your views here.
 
 
-class TodoCreateView(CreateView):
+class TodoCreateView(LoginRequiredMixin, CreateView):
     model = Todo
     fields = ('title', 'description', 'to_be_done')
     template_name = 'todo_new.html'
+    login_url = 'login'
 
     def form_valid(self, form):
         form.instance.creator = self.request.user
         return super().form_valid(form)
 
 
-class TodoListView(ListView):
+class TodoListView(LoginRequiredMixin, ListView):
     model = Todo
     template_name = 'todo_list.html'
+    login_url = 'login'
 
 
-class TodoDetailView(DetailView):
+class TodoDetailView(LoginRequiredMixin, DetailView):
     model = Todo
     template_name = 'todo_detail.html'
+    login_url = 'login'
 
 
-class TodoUpdateView(UpdateView):
+class TodoUpdateView(LoginRequiredMixin, UpdateView):
     model = Todo
     fields = ('title', 'description', 'to_be_done')
     template_name = 'todo_edit.html'
+    login_url = 'login'
 
 
-class TodoDeleteView(DeleteView):
+class TodoDeleteView(LoginRequiredMixin, DeleteView):
     model = Todo
     template_name = 'todo_delete.html'
     success_url = reverse_lazy('todo_list')
+    login_url = 'login'
